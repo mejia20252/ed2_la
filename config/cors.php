3be824b1ed75b50fa06bd86c402eb7fc6f1a7e50
Tu Nombre <tuemail@example.com>
2025-11-12 18:55:29 -0400

    Primer commit

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://ed2-re.onrender.com',
+        'http://localhost:5173', // Para desarrollo local
+    ],
 
     'allowed_origins_patterns' => [],
 
